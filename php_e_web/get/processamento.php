f7f1701bc16f87ed8bbd5d4be1682d8bd8788d79
Felipe Godoy <felipedegodoy.dev@gmail.com>
2024-11-08 09:04:00 -0300

    php e a web

---
+++ php_e_web/get/processamento.php
@@ -0,0 +1,7 @@
+<?php 
+
+    print_r($_GET);
+
+?>
+
+<h1>O seu nome é <?= $_GET['nome'] ?></h1>
---
+++ php_e_web/post/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="processamento.php" method="POST">
+        <div>
+            <input type="text" name="marca" placeholder="Marca do carro">
+        </div>
+        <div>
+            <input type="text" name="preco" placeholder="Preço do carro">
+        </div>
+        <div>
+            <input type="checkbox" name="opcionais[]" value="Teto solar"> Teto solar
+        </div>
+        <div>
+            <input type="checkbox" name="opcionais[]" value="Blindado"> Blindado
+        </div>
+        <div>
+            <input type="checkbox" name="opcionais[]" value="Película"> Película
+        </div>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
